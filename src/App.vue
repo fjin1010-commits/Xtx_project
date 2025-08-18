0f0be61445032144eab98a0a07e018b2b85557f8
Jim Jin <3315744065@qq.com>
2025-08-18 23:22:34 +1000

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <script setup>
-import { onMounted } from 'vue';
-import { useCounterStore } from './stores/counter';
-import { storeToRefs } from 'pinia';
-const counterStore = useCounterStore();
-const { count, name, doubleCount } = storeToRefs(counterStore); 
-onMounted(() => {
-  counterStore.fetchName();
-}); 
+
 
 
 </script>
 
 <template>
   <div>
-    <h1>Counter: {{ count }}</h1>
-    <h1>Double Counter: {{ counterStore.doubleCount }}</h1>
-    <h1>Name: {{ counterStore.name }}</h1>
-
-    <button @click="counterStore.increment">Increment</button>
-   
-
+    welcome to your Vue.js app!
   </div>
 </template>
 
