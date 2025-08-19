227dbd42f3090a0d5f36dcc8527d64d822c37914
Jim Jin <3315744065@qq.com>
2025-08-19 15:38:08 +1000

    static structure & font

--- src/views/layout/indexPage.vue
+++ src/views/layout/indexPage.vue
@@ -1,8 +1,16 @@
-<script></script>
+<script setup>
+import layoutNav from '@/components/layoutNav.vue';
+import layoutHeader from '@/components/layoutHeader.vue';
+import layoutFooter from '@/components/layoutFooter.vue';
+</script>
 
 <template>
   <div>
-    我是主页
+    <layoutNav />
+    <layoutHeader />
     <router-view />
+    <layoutFooter />
+    <!-- 这里可以放其他内容 -->
+    
   </div>
 </template>
