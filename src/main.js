7484ac229180e0f846c2678581660e13d0c84c7b
Jim Jin <3315744065@qq.com>
2025-08-20 14:45:32 +1000

    lazy img

--- src/main.js
+++ src/main.js
@@ -4,10 +4,25 @@ import { createPinia } from 'pinia'
 import 'element-plus/theme-chalk/src/index.scss'
 import '@/styles/common.scss'
 import router from './router'
+import { useIntersectionObserver } from '@vueuse/core'
 
 
-
+const app = createApp(App)
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(router).mount('#app')
+app.use(pinia).use(router).mount('#app')
+
+app.directive('img-lazy', {
+  mounted(el, binding) {
+  console.log('img-lazy directive mounted', el, binding.value);
+  useIntersectionObserver(
+    el,
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        el.src = binding.value
+      }
+    },
+  )
+}
+})
